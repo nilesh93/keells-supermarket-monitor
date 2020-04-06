c21719692e18a9ba513a18b27efc716e6dbb03b2
Nilesh Jayanandana <nilesh93.j@gmail.com>
2020-04-06 12:54:05 +0530

    fix hashmap

--- notify.sh
+++ notify.sh
@@ -7,43 +7,62 @@ UNAVAIL="Unavailable"
 RESPONSE=$(curl https://int.keellssuper.net/login)
 
 cities=( "Gampaha" "Kurunegala" "Wadduwa" )
-declare -A statuses
-            curl --header "Content-Type: application/json" \
-            --request POST \
-            --data "{'text':'keells availability monitor started'}" \
-            "$CRON_URL"
+declare -A status
+
+for city in "${cities[@]}"
+do
+    status[$city]="N/A"
+done
+
+curl --header "Content-Type: application/json" \
+ --request POST \
+ --data "{'text':'keells availability monitor started'}" \
+ "$CRON_URL"
 
 i=1
 while [ "$i" -ne 0 ]
 do
     for city in "${cities[@]}"
     do
-    echo "${city}"
+    echo "processing ${city}"
+    echo "${city} current status ${status[$city]}"
     RESULT=$(echo "$RESPONSE" | grep "$city")
 
     if [ "$RESULT" = "" ]; then
         echo "$city unavailable"
-        if [ statuses["$city"] != "$UNAVAIL" ]; then
+       
+        if [ ${status[$city]} != $UNAVAIL ]; then
             echo "Sending notification to city $city"
+
             curl --header "Content-Type: application/json" \
             --request POST \
             --data "{'text':'keells online ordering quota full for $city '}" \
             "$CRON_URL"
-            statuses["$city"]="$UNAVAIL"
+
+            status[$city]=$UNAVAIL
+            echo "$city saved status: ${status[$city]}"
+        else
+            echo "$city unavailable and already processed"
         fi
     else
         echo "$city available"
-        if [ statuses["$city"] != "$AVAIL" ]; then
+        if [ ${status[$city]} != $AVAIL ]; then
             echo "Sending notification to city $city"
+
             curl --header "Content-Type: application/json" \
             --request POST \
             --data "{'text':'keells online ordering available for $city. Login here: https://int.keellssuper.net/login'}" \
             "$CRON_URL"
-            statuses["$city"]="$AVAIL"
+
+            status[$city]=$AVAIL
+            echo "$city saved status: ${status[$city]}"
+        else
+            echo "$city available and already processed"
         fi
     fi
     done
-    sleep 10
+    echo "-----------------"
+    sleep 60
 done
  
 
